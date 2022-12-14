fb0e8ded4541a731bb26d344500b3d78920f8758
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-12-14 12:58:08 +0000

    tests: add integration test for build resumption using the `--resume` flag

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,1 +1,13 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import Iterator
+
+from pytest import fixture
+
 from kraken.core.test import kraken_project  # noqa: F401
+
+
+@fixture
+def tempdir() -> Iterator[Path]:
+    with TemporaryDirectory() as tempdir:
+        yield Path(tempdir)
