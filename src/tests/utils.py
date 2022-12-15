0ab2efee254e94436d4f2065dcd6ab0da1504bee
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-12-15 14:32:53 +0000

    lint

--- src/tests/utils.py
+++ src/tests/utils.py
@@ -1,10 +1,11 @@
 import os
 from contextlib import contextmanager
 from pathlib import Path
+from typing import Iterator
 
 
 @contextmanager
-def chdir_context(path: Path) -> None:
+def chdir_context(path: Path) -> Iterator[None]:
     cwd = Path.cwd()
     os.chdir(path)
     try:
