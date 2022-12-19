ddb9c71e46c09b7e5ec405d232a5c58231508134
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-12-19 22:51:00 +0000

    fix: add back `TaskRelationship` to `kraken.core.api`

--- src/kraken/core/api.py
+++ src/kraken/core/api.py
@@ -7,7 +7,16 @@ Users of Kraken should only import from this module.
 from kraken.core.system.context import Context, ContextEvent
 from kraken.core.system.project import Project, ProjectLoaderError
 from kraken.core.system.property import Property
-from kraken.core.system.task import BackgroundTask, GroupTask, Task, TaskSet, TaskStatus, TaskStatusType, VoidTask
+from kraken.core.system.task import (
+    BackgroundTask,
+    GroupTask,
+    Task,
+    TaskRelationship,
+    TaskSet,
+    TaskStatus,
+    TaskStatusType,
+    VoidTask,
+)
 
 __all__ = [
     "BackgroundTask",
@@ -18,6 +27,7 @@ __all__ = [
     "ProjectLoaderError",
     "Property",
     "Task",
+    "TaskRelationship",
     "TaskSet",
     "TaskStatus",
     "TaskStatusType",
