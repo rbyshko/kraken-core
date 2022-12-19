3f0eed259984cb8983b3201acf9def59b9d350c4
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-12-19 22:51:14 +0000

    release 0.11.4

--- src/kraken/core/__init__.py
+++ src/kraken/core/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.11.3"
+__version__ = "0.11.4"
 
 # NOTE(NiklasRosenstein): Imports from `kraken.core` directly are deprecated; instead you should import
 #   from `kraken.core.api`. This file will not be updated until the next release with breaking changes
